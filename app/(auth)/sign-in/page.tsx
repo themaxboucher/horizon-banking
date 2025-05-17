eb13629eb2d43e8775cdb014b53a2299a4d1abe1
Maxime Boucher <maxime@maximeboucher.com>
2025-05-17 11:28:08 -0600

    add initial file and folder structure

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SignIn = () => {
+  return (
+    <section className="flex-center size-full max-sm:px-6">Sign In</section>
+  );
+};
+
+export default SignIn;
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SignUp = () => {
+  return (
+    <section className="flex-center size-full max-sm:px-6">Sign Up</section>
+  );
+};
+
+export default SignUp;
