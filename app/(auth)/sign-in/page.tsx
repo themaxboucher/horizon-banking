bc777583e1d73c1dd47a8511149e6d24b7e60d76
Maxime Boucher <maxime@maximeboucher.com>
2025-05-18 10:10:34 -0600

    create auth UI

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import AuthForm from "@/components/AuthForm";
 
 const SignIn = () => {
   return (
-    <section className="flex-center size-full max-sm:px-6">Sign In</section>
+    <section className="flex-center size-full max-sm:px-6">
+      <AuthForm type="sign-in" />
+    </section>
   );
 };
 
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import AuthForm from "@/components/AuthForm";
 
-const SignUp = () => {
+const SignUp = async () => {
   return (
-    <section className="flex-center size-full max-sm:px-6">Sign Up</section>
+    <section className="flex-center size-full max-sm:px-6">
+      <AuthForm type="sign-up" />
+    </section>
   );
 };
 
